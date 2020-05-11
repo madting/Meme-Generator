995ad7e37ed9dab964e7a3e0bf9a7b3efbb40696
Mad Ting <samtastic1337@gmail.com>
2020-05-11 16:05:05 +0200

    Level 1 close to finish, ++ -- failed

--- src/meme.js
+++ src/meme.js
@@ -8,8 +8,8 @@ const apiUrl = "https://api.imgflip.com/get_memes"
 
 export default function Meme () {
  const [fetchData, fetchSet] = useState([])
- const [textInput1, textSet1] = useState("")
- const [textSave1, textValue1] = useState()
+ const [textInput1, textSet1] = useState()
+ const [textSave1, textValue1] = useState(textInput1)
  const [textInput2, textSet2] = useState()
  const [textSave2, textValue2] = useState(textInput2)
 
@@ -20,28 +20,47 @@ export default function Meme () {
      .catch(err => console.log(err))
  },[]);
 
-const handleSubmit1 = (event) =>{
+
+const handleSubmit = (event) =>{
 textValue1(textInput1)
 event.preventDefault();  
 textSet1("")
+textValue2(textInput2)
+event.preventDefault();  
+textSet2("")
 
 }  
 
 
 
+
 return (
 <div>
-<h1>Meme Generator</h1>
-<div>{textSave1}</div>
+<h1>_KnOw yOuR MeMe_</h1>
+
 <div className="Form">
-<form onClick={handleSubmit1}>    
-<label> This is your upper text
-<textarea type="text" value={textInput1} onChange={ e => textSet1(e.target.value)}/>
+<form>    
+<label> 
+<textarea placeholder="What goes up!"  cols="60" rows="2" type="text" value={textInput1} onChange={ e => textSet1(e.target.value)}/>
+{/* <div><button onClick={handleSubmit1}>Post your Meme</button></div> */}
+</label>
+</form>
+<form>    
+<label> 
+<textarea  placeholder="Must come down!" cols="60" rows="2" type="text" value={textInput2} onChange={ e => textSet2(e.target.value)}/>
+<div><button onClick={handleSubmit}>Post your Meme</button></div>
 </label>
 </form>
 </div>
-<div className="Fetched">
-<img src={fetchData.url}></img>
+
+<div className="Image">
+<div className="textform1">{textSave1}</div>
+
+<img src={fetchData.url}/>
+
+<div className="textform2">{textSave2}</div>
 </div>
+
+
 </div>
 )}
